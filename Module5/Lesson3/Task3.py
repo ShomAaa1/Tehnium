57e1ba5008fa4949692bc44ca53cd16922497cf1
Shoma10 <shomar10@rambler.ru>
2025-09-04 20:28:56 +0300

    add Praktikum1 to lesson4 module 5

--- Module5/Lesson3/Task3.py
+++ Module5/Lesson3/Task3.py
@@ -1,27 +1,56 @@
-import csv
 import json
 
-def csv_to_json(csv_filename: str, delimiter: str = ',', lineterminator: str = '\n') -> str:
+
+def csv_to_json(
+        csv_filename: str,
+        json_filename: str,
+        delimiter: str = ',',
+        line_sep: str = '\n'
+) -> None:
     """
-    Преобразует CSV в JSON строку
-    :param csv_filename: путь к CSV файлу
-    :param delimiter: разделитель значений (по умолчанию ',')
-    :param lineterminator: разделитель строк (по умолчанию '\n')
-    :return: JSON строка с отступами = 4
+    Конвертер CSV-файла в JSON-файл.
+    :param csv_filename: путь к входному CSV-файлу
+    :param json_filename: путь к выходному JSON-файлу
+    :param delimiter: символ-разделитель между значениями в CSV
+    :param line_sep: символ-разделитель строк
+    :return: None
     """
     with open(csv_filename, 'r', encoding='utf-8') as f:
-        reader = csv.DictReader(f, delimiter=delimiter, fieldnames=['товар', 'количество', 'цена'], lineterminator=lineterminator)
-        data = list(reader) # [{col: val}, {col: val}, ...]
+        csv_text = f.read()
 
-    # преобразуем список словарей в JSON строку
-    return json.dumps(data, indent=4, ensure_ascii=False)
+    # Убираем пробелы по краям и разбиваем на строки
+    lines = csv_text.strip().split(line_sep)
+    if not lines:
+        with open(json_filename, 'w', encoding="utf-8") as f:
+            json.dump([], f, indent=4, ensure_ascii=False)
+        return
+
+    # Заголовки
+    headers = lines[0].split(delimiter)
+
+    rows = []
+    for line in lines[1:]:
+        if not line.strip(): # пропускаем пустые строки
+            continue
+        values = line.split(delimiter)
+        row = {}
+        for col, val in zip(headers, values):
+            val = val.strip()
+            if val.isdigit():
+                row[col] = int(val)
+            else:
+                row[col] = val
+        rows.append(row)
+
+    # Записываем результат в JSON-файл
+    with open(json_filename, 'w', encoding='utf-8') as f:
+        json.dump(rows, f, indent=4, ensure_ascii=False)
 
 
 def main():
     csv_filename = 'prices.csv'
-    json_str = csv_to_json(csv_filename)
-
-    print(json_str)
+    json_filename = 'prices.json'
+    csv_to_json(csv_filename, json_filename)
 
 
 if __name__ == '__main__':
